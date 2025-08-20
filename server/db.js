87ed392e17cc6fe7787f2a9e273ef90e010dda1a
blueFlower11 <133530289+blueFlower11@users.noreply.github.com>
2025-08-21 00:34:45 +0200

    test connection to db

--- server/db.js
+++ server/db.js
@@ -1,8 +1,22 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: { rejectUnauthorized: false }
+// });
+
+// module.exports = pool;
+
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres", // or "mysql"
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
-module.exports = pool;
+module.exports = sequelize;
