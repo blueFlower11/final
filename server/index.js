b75ab1c2c193e272489b80ca20eca86860e0df91
blueFlower11 <133530289+blueFlower11@users.noreply.github.com>
2025-08-27 19:41:30 +0200

    friend

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ const io = new Server(server, {
   cors: {
     origin: allowedOrigins,
     methods: ["GET", "POST"],
-    credentials: flase,
+    credentials: false,
   },
   path: "/socket.io", 
 });
